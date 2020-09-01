4b07bb62506b5a71fdb8d8869f098392b46debc7
Nate Lynch <natelynch@Nates-MBP.lan>
2020-09-01 18:46:57 -0400

    add spell show page

--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,37 @@ const request = require("request");
 const rootURL = "https://www.dnd5eapi.co/api/spells";
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get('/', function (req, res, next) {
   request(`${rootURL}`, 
   function (err, response, body) {
     const spellData = JSON.parse(body);
-    res.render("index", { spellData });
+    res.render('index', { spellData });
   });
 });
+
+// GET Spell show page
+router.get('/:name', function(req, res, next) {
+  request(`${rootURL}/${req.params.name}`,
+  function(err, response, body) {
+    const spell = JSON.parse(body);
+    res.render('show', { spell })
+  })
+})
+
 router.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
 );
 router.get(
-  "/oauth2callback",
-  passport.authenticate("google", {
-    successRedirect: "/users",
-    failureRedirect: "/",
+  '/oauth2callback',
+  passport.authenticate('google', {
+    successRedirect: '/users',
+    failureRedirect: '/',
   })
 );
-router.get("/logout", function (req, res) {
+router.get('/logout', function (req, res) {
   req.logout();
-  res.redirect("/");
+  res.redirect('/');
 });
 
 module.exports = router;
