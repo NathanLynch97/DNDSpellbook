9b0ef17c05d5fae29ef10d2a5448d189bd0f7672
Nate Lynch <natelynch@Nates-MBP.lan>
2020-09-02 12:34:52 -0400

    show characters and form to create new

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,9 +1,25 @@
 const Character = require('../models/character');
 
 module.exports = {
-    index
+    index,
+    new: newCharacter,
+    create
 };
 
 function index(req, res, next) {
-    res.render('characters/index', {title: 'Your Characters'});
+    Character.find({}, function(err, characters) {
+        res.render('characters/index', {characters});
+    })
+}
+
+function newCharacter(req, res, next) {
+    res.render('characters/new');
+}
+
+function create(req, res) {
+    const character = new Character(req.body);
+    character.save(function(err) {
+        if (err) return res.render('characters/new');
+        res.redirect('/characters');
+    })
 }
--- routes/characters.js
+++ routes/characters.js
@@ -5,6 +5,8 @@ const charactersCtrl = require('../controllers/characters');
 /* GET users listing. */
 router.get('/', charactersCtrl.index);
 // GET new character form
-router.get('/characters/new', )
+router.get('/new', charactersCtrl.new);
+//POST new character
+router.post('/', charactersCtrl.create);
 
 module.exports = router;
