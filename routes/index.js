2b9770f36f7737d6311a0d50fe5064d98f551b60
Nate Lynch <natelynch@Nates-MBP.lan>
2020-09-03 15:58:13 -0400

    update for heroku login

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,6 @@ const rootURL = "https://www.dnd5eapi.co/api/spells";
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  // request(`${rootURL}`, function (err, response, body) {
-  //   const spellData = JSON.parse(body);
-  //   res.render("index", { spellData });
-  // });
   axios.get(`${rootURL}`).then(function(response) {
     console.log(response);
     spellData = response.data.results;
@@ -39,12 +35,6 @@ router.get("/logout", function (req, res) {
 });
 
 router.get("/:index", function (req, res, next) {
-  // axios.get(`${rootURL}/${req.params.index}`).then(function (response, body) {
-  //   const spell = JSON.parse(body);
-  //   res.render("show", { spell });
-  // }).catch(function(error) {
-  //   console.log(error);
-  // });
   axios.get(`${rootURL}/${req.params.index}`).then(function(response) {
     console.log(response.data);
     res.render('show', {spell: response.data});
